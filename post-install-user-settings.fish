bb646a912c966d0d0acdbf6b002b9e574d41f6e9
oryon-dominik <oryon@cyberise.de>
2021-12-04 12:07:50 +0100

    user settings fish

--- post-install-user-settings.fish
+++ post-install-user-settings.fish
@@ -6,6 +6,7 @@ mkdir -p ~/.config
 # fish
 mkdir -p ~/.config/fish/functions
 xh --download https://raw.githubusercontent.com/oryon-dominik/dotfiles/master/common/fish/config.fish --output ~/.config/fish/config.fish
+xh --download https://raw.githubusercontent.com/oryon-dominik/dotfiles/master/common/fish/aliases.fish --output ~/.config/fish/aliases.fish
 xh --download https://raw.githubusercontent.com/oryon-dominik/dotfiles/master/common/fish/functions/last_command_as_sudo.fish --output ~/.config/fish/functions/last_command_as_sudo.fish
 
 # alacritty
@@ -20,3 +21,28 @@ xh --download https://raw.githubusercontent.com/oryon-dominik/dotfiles/master/co
 
 # starship
 xh --download https://raw.githubusercontent.com/oryon-dominik/dotfiles/master/common/starship/starship.toml --output ~/.config/starship.toml
+
+# Python
+# fallback:
+# nix-env -iA nixos.python310
+# nix-env -iA nixos.python310Packages.pip
+# nix-env -iA nixos.python310Packages.argcomplete
+
+# git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+# pyenv install 3.10.0
+# pyenv global 3.10.0
+# pyenv rehash
+# python -m pip install --user pipx
+# python -m pipx ensurepath
+# python -m pipx completions
+# python -m pipx install virtualfish
+# vf install compat_aliases
+
+# curl -sSL https://install.python-poetry.org | python3 -
+
+# fish extensions
+# curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+# fisher install jethrokuan/fzf  # fuzzy finder :)
+# fisher install dracula/fish  # dracula colors
+# fisher install daenney/pyenv  # pyenv support for fish
+# fisher install edc/bass  # bash wrapper for fish, -> $ bass <bash commmand>
